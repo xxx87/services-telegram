370498a8e1fa6485e646fd168939f34d7ed3fe0e
ii87 <ii87@ukr.net>
2020-08-12 21:58:42 +0300

    added keyboard

--- app.js
+++ app.js
@@ -32,6 +32,26 @@ app.listen(port, () => {
   console.log(`Express server is listening on ${port}`);
 });
 
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome", {
+    reply_markup: {
+      keyboard: [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]]
+    }
+  });
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
 // Just to ping!
 bot.on("message", (msg) => {
   bot.sendMessage(msg.chat.id, "I am alive!");
