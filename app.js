170817dc1f167e4bf3615b378466f9175aa9ef50
i.isaev <i.isaev@pronet.ua>
2020-08-18 13:59:14 +0300

    CR

--- app.js
+++ app.js
@@ -210,10 +210,10 @@ function startDevMode(bot) {
 
 async function startProdMode(bot) {
   await bot.telegram.setWebhook(`${urlWebHook}/bot${TOKEN}`);
+  await bot.telegram.startWebhook(`/${TOKEN}`);
   // await bot.startWebhook(`/${process.env.TELEGRAM_TOKEN}`, tlsOptions, +process.env.WEBHOOK_PORT);
   const webhookStatus = await bot.telegram.getWebhookInfo();
   console.log("Webhook status1 ", webhookStatus);
-  console.log("Webhook status2 ", bot);
 }
 
 app.listen(port, () => {
